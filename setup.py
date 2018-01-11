4d48a5f6c986534bff7f1f493b1f1420e1a36907
Ryan Morshead <ryan.morshead@gmail.com>
2018-01-11 14:17:38 -0800

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 #-----------------------------------------------------------------------------
 
 project = "jotting"
-version = "0.0.1"
+version = "0.0.2"
 author = "Ryan Morshead"
 email = "ryan.morshead@gmail.com"
 summary = "causally related logging messages"
