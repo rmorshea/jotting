5fdf927306831e4cd2fbf5858bdb117c7d38fdf5
Ryan Morshead <ryan.morshead@gmail.com>
2018-01-11 18:05:26 -0800

    more in setup

--- setup.py
+++ setup.py
@@ -10,11 +10,28 @@ from setuptools import find_packages
 # Project
 #-----------------------------------------------------------------------------
 
+url = "https://github.com/rmorshea/jotting"
 project = "jotting"
 version = "0.1.0"
 author = "Ryan Morshead"
 email = "ryan.morshead@gmail.com"
 summary = "causally related logging messages"
+
+description = """
+Jotting
+-------
+
+Logs that explain when, where, and why things happen.
+
+``jotting`` is a log system for Python 3 that can be used to record the causal
+history of an asynchronous or distributed system. These histories are composed
+of actions which, once "started", will begin "working", potentially spawn other
+actions, and eventually end as a "success" or "failure". In the end you're left
+with a breadcrumb trail of information that you can use to squash bugs with
+minimal boilerplate.
+
+Jotting was heavily inspired by ``eliot`` (https://eliot.readthedocs.io/).
+"""
 
 #-----------------------------------------------------------------------------
 # Packages
@@ -39,14 +56,28 @@ else:
     requires = ["psutil", "funcsigs"]
 
 parameters = dict(
+    url=url,
     name=project,
     version=version,
     author=author,
     author_email=email,
     description=summary,
+    long_description=description,
     packages=find_packages(),
     python_requires = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requries=requires,
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
 
 #-----------------------------------------------------------------------------
